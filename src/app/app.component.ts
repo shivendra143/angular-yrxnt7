8120a9c057ca02d40b080b83308e1b677b8d422b
shivendra143 <shivendrachaudhary05@gmail.com>
2020-03-17 03:44:37 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+  
+ ngOnInit(){
+   this.updateArra(this.acctData, this.balance, this.sortby, this.filter); 
+ }
+acctData:any = [
+{
+"acctNum": "AAA - 1234",
+"user": "Alice"
+},
+{
+"acctNum": "AAA - 5231",
+"user": "Bob"
+},
+{
+"acctNum": "AAA - 9921",
+"user": "Alice"
+},
+{
+"acctNum": "AAA - 8191",
+"user": "Alice"
+}
+];
+balance:any = {
+"AAA - 1234": 4593.22,
+"AAA - 9921": 0,
+"AAA - 5231": 232142.5,
+"AAA - 8191": 4344
+};
+ sortby:string = 'acctNum';
+ filter:string = 'Alice';
+ account:any =[];
+ users:any;
+updateArra(acctData, balance,opt,filter){
+
+      this.account =[];
+     for(let i=0; i<acctData.length; i++){
+       this.account[i] = {user:acctData[i].user, acctNum:acctData[i].acctNum, balance:balance[acctData[i].acctNum]};
+     }
+  console.log(opt)
+   
+  if(opt=="user"){
+    this.account.sort(function (a, b) {
+    return a.user.localeCompare(b.user);
+  });
+  }
+   if(opt=="acctNum"){
+    this.account.sort(function (a, b) {
+    return a.acctNum.localeCompare(b.acctNum);
+  });
+  }
+   if(opt=="balance"){
+    this.account.sort(function (a, b) {
+    return a.balance - b.balance;
+  });
+  }
+ // console.log(filter)
+   
+  if(filter!=""){
+    this.users =  this.account.filter(function(acc) {
+	return acc.user == filter;
+     });
+   }                                       
+   console.log(this.account)
+   console.log(this.users);
+  }
   
 }
